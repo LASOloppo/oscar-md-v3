1535ecb18170ae5bb82843e71c7488a0eddc2d85
LASOloppo <111619867+LASOloppo@users.noreply.github.com>
2022-09-01 20:06:05 +0800

    Update menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -179,17 +179,17 @@ let handler = async (m, { conn, usedPrefix: _p }) => {
                         hydratedTemplate: {
                             imageMessage: message.imageMessage,
                             hydratedContentText: text.trim(),
-                            hydratedFooterText:'                『 𝑱𝑨𝑹𝑶𝑻 𝑶𝑭𝑭𝑪 』',
+                            hydratedFooterText:'                『 AtsairBot-Md 』',
                             hydratedButtons: [{
                               urlButton: {
                                     displayText: '𝙂𝙧𝙤𝙪𝙥 🌏',
-                                    url: 'https://chat.whatsapp.com/IqZke0cAG6G3iVsnB9myfL'
+                                    url: 'https://chat.whatsapp.com/LC2ZNYJi7iB4zNp7juywV0'
                                 }
                                 
                             }, {
                               urlButton: {
                                     displayText: '𝙔𝙤𝙪𝙏𝙪𝙗𝙚 📺',
-                                    url: 'https://youtube.com/channel/UCW7iXlE7TgvJMIXQck4NYBQ'
+                                    url: 'https://youtube.com/channel/UC3VSUWzMu_tk61XIiH1GgYg'
                                 }
                             }, {
                                 quickReplyButton: {
